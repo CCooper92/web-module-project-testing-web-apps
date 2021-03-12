3d56e63abcfb9b833e1d7850c3e76959feefaa05
CCooper92 <am10ac17@gmail.com>
2021-03-11 20:51:12 -0800

    not finished

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -3,17 +3,20 @@ import {render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import ContactForm from './ContactForm';
+import { act } from 'react-dom/test-utils';
 
 test('renders without errors', ()=>{
-    
+    render(<ContactForm />);
 });
 
 test('renders the contact form header', ()=> {
-    
+    render(<ContactForm />)
+    const header = screen.queryByText
 });
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
-    
+    const firstNameError = screen.getByRole
+    // expect(screen.queryByText(/First name/i))
 });
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
@@ -33,7 +36,23 @@ test('renders "lastName is a required field" if an last name is not entered and 
 });
 
 test('renders all firstName, lastName and email text when submitted. Does NOT render message if message is not submitted.', async () => {
-    
+ render(<ContactForm />)
+
+ const firstNameText = screen.getByLabelText("First Name")
+ userEvent.type(firstNameInput, "Courtney")
+
+ const lastNameText = screen.getByLabelText("Last Name")
+ userEvent.type(lastNameInput, "Cooper")
+
+ const emailText = screen.getByLabelText("Email")
+ userEvent.type(emailInput, "am10ac17@gmail.com")
+
+ const button = screen.getByRole("button");
+ userEvent.click(button);
+  
+ const formSubmission = screen.getByText(/you submitted/i);
+ console.log(formSubmission);
+ expect(formSubmission).toBeInTheDocument();
 });
 
 test('renders all fields text when all fields are submitted.', async () => {
